964bf87921aaa633502cca9294c22c32c5e09f5b
Genet-Abay <genetabay.s@gmail.com>
2026-09-09 16:11:53 +0200

    null values for code agent removed

--- app.py
+++ app.py
@@ -66,15 +66,23 @@ model = InferenceClientModel(
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
     
+# agent = CodeAgent(
+#     model=model,
+#     tools=[final_answer, get_current_time_in_timezone, get_weather], ## add your tools here (don't remove final answer)
+#     max_steps=6,
+#     verbosity_level=1,
+#     grammar=None,
+#     planning_interval=None,
+#     name=None,
+#     description=None,
+#     prompt_templates=prompt_templates
+# )
+
 agent = CodeAgent(
     model=model,
-    tools=[final_answer, get_current_time_in_timezone, get_weather], ## add your tools here (don't remove final answer)
+    tools=[final_answer, get_current_time_in_timezone, get_weather],
     max_steps=6,
     verbosity_level=1,
-    grammar=None,
-    planning_interval=None,
-    name=None,
-    description=None,
     prompt_templates=prompt_templates
 )
 
